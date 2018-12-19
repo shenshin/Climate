7ac2d90e4538547f1a3f79f2057bbb22a5fcbeee
Ales Shenshin <shenshin@me.com>
2018-12-19 04:36:53 +0300

    Устанавливаю Pods (Alamofire, SwiftyJason, ProgressHUD) и настраиваю Location Manager для запроса разрешения на использование текущего местоположения.

--- Clima/WeatherViewController.swift
+++ Clima/WeatherViewController.swift
@@ -7,18 +7,18 @@
 //
 
 import UIKit
+import CoreLocation
 
 
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, CLLocationManagerDelegate {
     
     //Constants
     let WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather"
-    let APP_ID = "e72ca729af228beabd5d20e3b7749713"
-    /***Get your own App ID at https://openweathermap.org/appid ****/
+    let APP_ID = "39ed13c579ea7a1447e1b92059983746"
     
 
     //TODO: Declare instance variables here
-    
+    let locationManager : CLLocationManager = CLLocationManager()
 
     
     //Pre-linked IBOutlets
@@ -32,8 +32,9 @@ class WeatherViewController: UIViewController {
         
         
         //TODO:Set up the location manager here.
-    
-        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        locationManager.requestWhenInUseAuthorization()
         
     }
     
